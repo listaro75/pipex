790a3470847a2ddf2c6eccb46d5c801cc795ce37
luda-cun <lucien.dacunha@gmal.com>
2025-03-25 14:40:18 +0100

    version 0.4 | verification avec copilote

--- children.c
+++ children.c
@@ -6,7 +6,7 @@
 /*   By: luda-cun <luda-cun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 12:08:26 by luda-cun          #+#    #+#             */
-/*   Updated: 2025/03/24 12:38:27 by luda-cun         ###   ########.fr       */
+/*   Updated: 2025/03/25 14:38:10 by luda-cun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,16 +30,16 @@ void	children_processe2(int pipe1[2], int fd[2], char **cmd2, char **envp)
 	close(pipe1[1]);
 	if (execve(cmd2[0], cmd2, envp) == -1)
 	{
-		perror("execve cmd2");
+		perror(cmd2[0]);
 		exit(EXIT_FAILURE);
 	}
 }
 
 void	close_fd(int pipe1[2], int fd[2])
 {
-	if (fd[0] > 0)
+	if (fd[0] >= 0)  // On vérifie que fd[0] est bien valide avant de le fermer
 		close(fd[0]);
-	if (fd[1] > 0)
+	if (fd[1] >= 0)  // Même chose pour fd[1]
 		close(fd[1]);
 	close(pipe1[0]);
 	close(pipe1[1]);
@@ -87,14 +87,17 @@ void	create_children(char **cmd1, char **cmd2, char **envp, char **av)
 	int	pid2;
 
 	init_fd_pipe(fd, pipe1, av);
-	pid1 = fork();
-	if (pid1 < 0)
+	if (fd[0] > 0)
 	{
-		perror("fork");
-		exit(EXIT_FAILURE);
+		pid1 = fork();
+		if (pid1 < 0)
+		{
+			perror("fork");
+			exit(EXIT_FAILURE);
+		}
+		if (pid1 == 0)
+			children_processe1(pipe1, fd, cmd1, envp);
 	}
-	if (pid1 == 0)
-		children_processe1(pipe1, fd, cmd1, envp);
 	pid2 = fork();
 	if (pid2 < 0)
 	{
@@ -104,6 +107,6 @@ void	create_children(char **cmd1, char **cmd2, char **envp, char **av)
 	if (pid2 == 0)
 		children_processe2(pipe1, fd, cmd2, envp);
 	close_fd(pipe1, fd);
-	waitpid(pid1, NULL, 0);
-	waitpid(pid2, NULL, 0);
+	// waitpid(pid1, NULL, 0);
+	// waitpid(pid2, NULL, 0);
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: luda-cun <luda-cun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 11:15:37 by luda-cun          #+#    #+#             */
-/*   Updated: 2025/03/24 11:51:44 by luda-cun         ###   ########.fr       */
+/*   Updated: 2025/03/25 14:37:17 by luda-cun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,18 @@ void	execution(char *cmd1, char *cmd2, char **argv, char **envp)
 	char	**cmdopt1;
 	char	**cmdopt2;
 
-	cmdopt1 = ft_split(argv[2], ' ');
-	cmdopt2 = ft_split(argv[3], ' ');
-	cmdopt1 = change_tab(cmdopt1, cmd1);
-	cmdopt2 = change_tab(cmdopt2, cmd2);
+	cmdopt1 = NULL;
+	cmdopt2 = NULL;
+	if (cmd1)
+	{
+		cmdopt1 = ft_split(argv[2], ' ');
+		cmdopt1 = change_tab(cmdopt1, cmd1);
+	}
+	if (cmd2)
+	{
+		cmdopt2 = ft_split(argv[3], ' ');
+		cmdopt2 = change_tab(cmdopt2, cmd2);
+	}
 	create_children(cmdopt1, cmdopt2, envp, argv);
 	return (free_tab(cmdopt1), free_tab(cmdopt2));
 }
@@ -36,7 +44,7 @@ char	**ft_cmdtouch(char *cmd, char *opt)
 	return (cmdtouch);
 }
 
-void	create_outfile(char **av, char **envp, char **paths)
+void	create_outfile(char **av, char **envp, char **paths, int fd)
 {
 	int		pid1;
 	char	*cmdtouch;
@@ -44,6 +52,7 @@ void	create_outfile(char **av, char **envp, char **paths)
 
 	cmdtouch = verif_ex(paths, "touch");
 	outfile = ft_cmdtouch(cmdtouch, av[4]);
+	close(fd);
 	pid1 = fork();
 	if (pid1 < 0)
 	{
@@ -76,13 +85,13 @@ int	main(int argc, char **argv, char **envp)
 	paths = the_paths(envp);
 	fd[1] = open(argv[4], O_RDWR);
 	if (fd[1] == -1)
-		create_outfile(argv, envp, paths);
+		create_outfile(argv, envp, paths, fd[1]);
 	pathcmd1 = verif_ex(paths, argv[2]);
 	if (pathcmd1 == NULL)
-		return (free_tab(paths), perror("cmd1"), 1);
+		perror(argv[2]);
 	pathcmd2 = verif_ex(paths, argv[3]);
 	if (pathcmd2 == NULL)
-		return (free(pathcmd1), free_tab(paths), perror("cmd2"), 1);
+		perror(argv[3]);
 	execution(pathcmd1, pathcmd2, argv, envp);
 	close(fd[0]);
 	close(fd[1]);
